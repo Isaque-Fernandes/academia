7c0f13a838582346b2f725bc38ec32070cf02e65
DanielSRS013 <danielsrs.mkd@gmail.com>
2022-03-19 23:32:33 -0300

    feat: criacao_footer.css_e_implementacao_do_footer_na_view_usuario_form.php

--- php/usuario_formulario.php
+++ php/usuario_formulario.php
@@ -3,6 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../resources/css/style.css" rel="stylesheet"/>
+    <link href="../resources/css/footer.css" rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Cadastro Usuários</title>
 </head>
@@ -39,12 +40,12 @@
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Ficha Treino
                         </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="./fichaTreino_formulario.php">Cadastro</a></li>
-                            <li><a class="dropdown-item" href="./fichaTreino_listagem.php">Listagem</a></li>
+                        <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item text-light" href="./fichaTreino_formulario.php">Cadastro</a></li>
+                            <li><a class="dropdown-item text-light" href="./fichaTreino_listagem.php">Listagem</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -100,13 +101,27 @@
     </div>
 
     <!--Footer-->
-    <footer class="footer bg-dark">
+    
+    <footer>
         <div class="container-footer">
-            <div class="container-text-footer">
-
+            <div class="text text-1">
+                <ul>
+                    <li><h2>Instrutor</h2></li>
+                    <li><a href="./instrutor_formulario.php">Cadastro</a></li>
+                    <li><a href="./instrutor_listagem.php">Lista</a></li>
+                </ul>
+            </div>
+            <div class="text_img">
+                <a href="./index.php"><img src="../resources/img/logo1.png" alt="logomarca"></a>
+            </div>
+            <div class="text">
+                <ul>
+                    <li><h2>Usuario</h2></li>
+                    <li><a href="./usuario_formulario.php">Cadastro</a></li>
+                    <li><a href="./usuario_listagem.php">Lista</a></li>
+                </ul>
             </div>
         </div>
-
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
