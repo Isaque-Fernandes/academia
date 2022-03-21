7fd5d25506f4910c822fbe2fb72abefda4ce32d4
DanielSRS013 <danielsrs.mkd@gmail.com>
2022-03-21 17:52:55 -0300

    fix: centralizando_acesso_permitido

--- php/cadastro_sistema.php
+++ php/cadastro_sistema.php
@@ -18,9 +18,20 @@ if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['email']) == true
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
-
+    <style>
+        div{
+            width: 100%;
+            height: 100vh;
+            display: flex; 
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
 <body>
-    <h1>Acesso Permitido</h1>
+    <div>
+        <h1>Acesso Permitido</h1>
+        
+    </div>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
