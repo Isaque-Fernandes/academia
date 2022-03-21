1b5d5b125eb9d47ace13de2e3dd3a6de9e4096ed
DanielSRS013 <danielsrs.mkd@gmail.com>
2022-03-21 18:07:45 -0300

    fix: justificando_footer_no_final_da_pagina_e_atualizando_cadastro_sistema

--- php/cadastro_sistema.php
+++ php/cadastro_sistema.php
@@ -16,21 +16,25 @@ if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['email']) == true
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/usuarioformulario.css" />
     <title>Document</title>
 </head>
-    <style>
-        div{
-            width: 100%;
-            height: 100vh;
-            display: flex; 
-            justify-content: center;
-            align-items: center;
-        }
-    </style>
 <body>
-    <div>
-        <h1>Acesso Permitido</h1>
-    </div>
+
+
+<section class="backgroundcontainer">
+        <div class="background">
+        <div class="caixamae">
+                <div class="login-page">
+                <div class="form">
+    <legend>Acesso Permitido</legend>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+    
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
