1cb4f348e6edcc472c61751dd2167c2cf76944c8
Isaque-Fernandes <isaquefernandesuser@gmail.com>
2022-03-21 10:54:00 -0300

    fix: all files

--- php/cadastro_formulario.php
+++ php/cadastro_formulario.php
@@ -7,46 +7,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../css/style.css" />
-    <link rel="stylesheet" type="text/css" href="../css/instrutorformulario.css" />
-    <link rel="stylesheet" type="text/css" href="../css/footer.css" />
     <title>Document</title>
 </head>
 
 <body>
 
-<!-- INÍCIO NAVBAR -->
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="./index.php"><img src="../img/logo1.png" alt="..." height="100px"></a>
+            <a class="navbar-brand" href="./index.php">Standard Fit</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active text-light" aria-current="page" href="./index.php">Home</a>
+                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Instrutores
                         </a>
-                        <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item text-light" href="./instrutor_formulario.php">Cadastro</a></li>
-                            <li><a class="dropdown-item text-light" href="./instrutor_redirecionador_listagem.php">Listagem</a></li>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="./instrutor_formulario.php">Cadastro</a></li>
+                            <li><a class="dropdown-item" href="./instrutor_redirecionador_listagem.php">Listagem</a></li>
                         </ul>
                     </li>
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Usuários
                         </a>
-                        <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item text-light" href="./usuario_formulario.php">Cadastro</a></li>
-                            <li ><a class="dropdown-item text-light" href="./usuario_redirecionador_listagem.php">Lista</a></li>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="./usuario_formulario.php">Cadastro</a></li>
+                            <li><a class="dropdown-item" href="./usuario_redirecionador_listagem.php">Lista</a></li>
                         </ul>
                     </li>
-                    
+
                 </ul>
                 <form class="d-flex">
                     <input class="form-control me-2" name="instrutor_txt" type="search" placeholder="O que você procura?" aria-label="Search">
@@ -55,51 +51,22 @@
                 </form>
             </div>
         </div>
-</nav>
-<!-- FIM DO NAVBAR -->
+    </nav>
 
+    <h1>Acesso Restrito</h1>
 
+    <form action="./cadastro_teste.php" method="post">
+        <p>
+            <label for="">Email</label>
+            <input class="form-control" type="text" name="email_txt" placeholder="Email">
+        </p>
+        <p>
+            <label for="">Senha</label>
+            <input type="password" name="senha_txt" class="form-control" placeholder="Senha">
+        </p>
+        <input type="submit" value="Enviar" class="btn btn-dark" name="botao_txt">
 
-<!-- INICIO MEIO DO SITE -->
-<section class="backgroundcontainer">
-        <div class="background">
-
-
-            <!-- IMAGEM BACKGROUND -->
-            <style>
-                body
-                {
-                    background: url('../img/6.jpg') no-repeat;
-                }
-            </style>
-            <!-- FIM IMAGEM BACKGROUND -->
-
-            <div class="caixamae">
-                <div class="login-page">
-                <div class="form">
-
-
-                    <legend>Acesso Restrito</legend>
-
-                    <form class="register-form" action="./cadastro_teste.php" method="post">
-                        
-
-                        <input type="text" name="email_txt" id="email_id" placeholder="Digite Seu Email" required>
-                        <input  type="password" name="senha_txt" id="senha_id" placeholder="Informe sua Senha" required>
-                        <button class="btn btn-dark" type="submit" name="botao_txt">Entrar</button>
-                    </form>
-                </div>
-                </div>
-            </div>              <!--FIM FORMULÁRIO DE CADASTRO-->
-        </div>
-</section>
-<!-- FIM MEIO DO SITE -->
-
-
-
-
-       
-
+    </form>
 
         <!-- INICIO FOOTER -->
 <footer>
