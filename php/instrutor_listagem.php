2bd3ecb6e6fa517207da2522d5ed454b0e9e71c9
jhonatan salgueiro <jhonatansalgueiro2002@gmail.com>
2022-03-21 01:20:50 -0300

    atualizado com o footer as paginas cadastro_formulario.php , instrutor_listagem.php e usuario_listagem.php

--- php/instrutor_listagem.php
+++ php/instrutor_listagem.php
@@ -132,7 +132,30 @@ if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['email']) == true
     </div>
 </section>
 
+    <!-- INICIO FOOTER -->
+<footer>
+    <div class="container-footer">
+        <div class="text text-1">
+            <ul>
+                <li><h2>Instrutor</h2></li>
+                <li><a href="./instrutor_formulario.php">Cadastro</a></li>
+                <li><a href="./instrutor_listagem.php">Lista</a></li>
+            </ul>
+        </div>
+        <div class="text_img">
+                <a href="./index.php"><img src="../img/logo1.png" height="100px" alt="logomarca"></a>
+        </div>
+        <div class="text">
+            <ul>
+                <li><h2>Usuario</h2></li>
+                <li><a href="./usuario_formulario.php">Cadastro</a></li>
+                <li><a href="./usuario_listagem.php">Lista</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
 
+    <!-- FIM DO FOOTER -->
 
 
 
