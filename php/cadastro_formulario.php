f1aba49e0e538acea31b54d77f4a64f16e406215
dan-cardoso <daniel.cardoso.90@hotmail.com>
2022-03-21 13:40:59 -0300

    fix: correção dos navbar, dropdown branco

--- php/cadastro_formulario.php
+++ php/cadastro_formulario.php
@@ -15,11 +15,11 @@
 
 <body>
 
-<!-- INÍCIO NAVBAR -->
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <!-- INÍCIO NAVBAR -->
+    <nav class="navbar navbar-expand-md navbar-light bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="./index.php"><img src="../img/logo1.png" alt="..." height="100px"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
