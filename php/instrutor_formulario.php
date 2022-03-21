3c913ae890703af3f391622a59c1a2668e571e98
dan-cardoso <daniel.cardoso.90@hotmail.com>
2022-03-21 12:58:05 -0300

    fix: correcao instrutor_formulario.php

--- php/instrutor_formulario.php
+++ php/instrutor_formulario.php
@@ -96,7 +96,7 @@ if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['email']) == true
 
                     <legend>Cadastro de Usuário</legend>
             
-                    <form class="register-form" action="./usuario_registro.php" method="post">
+                    <form class="register-form" action="./instrutor_registro.php" method="post">
 
                         <input type="text" name="cpf_txt" id="cpf_id" placeholder="CPF" required>
                         <input  type="text" name="nomeUsuario_txt" id="nomeUsuario_id" placeholder="Informe o Nome" required>
